78b69c2086e2f26cff36b2c6c36f8e3423432ccb
Alexandr <zdesev-aleksandr@mail.ru>
2022-02-06 22:22:24 +0400

    weather app start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+
 import './App.css';
 
+const api = {
+  key: "95dcccab70174fc2926a2e3be7f6a95a",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = async (event) => {
+    if (event.key === "Enter") {
+       
+      let res = await fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`);
+      let json = await res.json();
+      setWeather(json);
+      setQuery('');
+      console.log(json);
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["Jan", "Fev", "Mar", "Fev", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
+    let day = days[d.getDay()];//0-6
+    let date = d.getDate();//1-31
+    let month = months[d.getMonth()];//0-11
+    let year = d.getFullYear();//2022
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="city" onKeyPress={search} onChange={(e) => setQuery(e.target.value)} value={query} />
+      {(typeof weather.main !== 'undefined') ? (<div><div>{weather.name}, {weather.sys.country}</div>
+      <div>{dateBuilder(new Date())}</div>
+        <div>{Math.round(weather.main.temp)}{'\xB0'}</div>
+      <div>{weather.weather[0].main}</div></div>) : null}
+     
     </div>
   );
 }
